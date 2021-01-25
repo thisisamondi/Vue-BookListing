252492eb61e85296d053d4b976ef1c9443d00fe1
thisisamondi <67878188+thisisamondi@users.noreply.github.com>
2021-01-25 17:03:42 +0100

    fix

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,7 +2,7 @@
   <div>
       <h1>{{title}}</h1>
       <ul>
-       <li v-for: book in books>{{book.title}}:{{book.author}}</li>
+       <li v-for="book in books">{{book.title}}:{{book.author}}</li>
     </ul>
       
     </div>
@@ -14,9 +14,9 @@ data () {
     return {
         title: "All Books",
         books: [
-    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
-    {title: 'American Gods', author: 'Neil Gaiman'},
-    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+            {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+            {title: 'American Gods', author: 'Neil Gaiman'},
+            {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
 ]
     }
 }
